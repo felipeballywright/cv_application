2443c0d2f04cc462da4002e0638fffd1a1f7cf0e
felipeballywright <felipeballywright@gmail.com>
2025-07-03 20:06:09 -0300

    DAY 2 CV NIGHT

--- src/education.jsx
+++ src/education.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-// MAKE A RENDER ARRAY WITH ALL OF THE SECTIONS
-// THE FUNCTIONALITY SHOULD ADD THEM AND DELETE THEM FROM THE ARRAY
-// THEN AT THE END OF THE FUNCTION MAP AND RETURN THIS ARRAY (FOR IT TO BE RENDERED)
-// DON'T TRY TO MODIFY THE RENDER ITSELD, WORK ON THE ARRAY (SO YOU CAN USE ITERATION AND OTHER TOOLS)
+// Store an array of unique IDs/objects.
+// Use .map() to render <EducationForm /> based on that array.
+// On delete, filter out the ID.
+// Add new sections by appending a new ID.
 
 function Education({title, school, date}){
     return(
@@ -15,86 +15,105 @@ function Education({title, school, date}){
     )
 }
 
-function EducationForm(){
-    const [titleInput, setTitleInput] = useState('');
-    const [schoolInput, setSchoolInput] = useState('');
-    const [dateInput, setDateInput] = useState('');
-    const [isSubmitted, setIsSubmitted] = useState(false);
+export function HandleEducation(){
+    console.log("Running HandleEducation");
+    const educationData = [];
 
     function handleSubmit(){
-        if(!isSubmitted){
-            setIsSubmitted(true);
-        } else if(isSubmitted){
-            setIsSubmitted(false);
-        }
+
     }
 
-    function handleTitle(event){
-       setTitleInput(event.target.value);
-    }
-
-    function handleSchool(event){
-       setSchoolInput(event.target.value);
-    }
-
-    function handleDate(event){
-       setDateInput(event.target.value);
-    }
-
-    if(isSubmitted){
-        return(
-            <>
-              <Education
-                title={titleInput}
-                school={schoolInput}
-                date={dateInput}
-              />
-              <button onClick={handleSubmit}>Edit</button>
-            </>
-        )
-    }
-    
     return(
         <form onSubmit={handleSubmit}>
             <label htmlFor="title-input">Title</label>
-            <input onChange={handleTitle} id="title-input" value={titleInput}></input>
+            <input id="title-input"></input>
             <label htmlFor="school-input">School</label>
-            <input onChange={handleSchool} id="school-input" value={schoolInput}></input>
+            <input id="school-input"></input>
             <label htmlFor="date-input">Date</label>
-            <input onChange={handleDate} id="date-input" value={dateInput}></input>
+            <input id="date-input"></input>
             <button>Submit</button>
         </form>
     )
 }
 
-export function EducationRender(){
-    const [render, setRender] = useState([
-        <div id={crypto.randomUUID()}>
-            <EducationForm></EducationForm>
-            <button onClick={deleteSection}>Delete</button>
-        </div>
-    ]);
+// function EducationForm(){
+//     const [titleInput, setTitleInput] = useState('');
+//     const [schoolInput, setSchoolInput] = useState('');
+//     const [dateInput, setDateInput] = useState('');
+//     const [isSubmitted, setIsSubmitted] = useState(false);
 
-    function addSection(){
-        setRender(render.push(
-            <div id={crypto.randomUUID()}>
-                <EducationForm></EducationForm>
-                <button onClick={deleteSection}>Delete</button>
-            </div>
-        ));
-    }
+//     function handleSubmit(){
+//         if(!isSubmitted){
+//             setIsSubmitted(true);
+//         } else if(isSubmitted){
+//             setIsSubmitted(false);
+//         }
+//     }
 
-    function deleteSection(event){
-        // console.log("deleteSection says:", event.target.parentElement.id);
-        for (let index = 0; index < render.length; index++) {
+//     function handleTitle(event){
+//        setTitleInput(event.target.value);
+//     }
+
+//     function handleSchool(event){
+//        setSchoolInput(event.target.value);
+//     }
+
+//     function handleDate(event){
+//        setDateInput(event.target.value);
+//     }
+
+//     if(isSubmitted){
+//         return(
+//             <>
+//               <Education
+//                 title={titleInput}
+//                 school={schoolInput}
+//                 date={dateInput}
+//               />
+//               <button onClick={handleSubmit}>Edit</button>
+//             </>
+//         )
+//     }
+    
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <label htmlFor="title-input">Title</label>
+//             <input onChange={handleTitle} id="title-input" value={titleInput}></input>
+//             <label htmlFor="school-input">School</label>
+//             <input onChange={handleSchool} id="school-input" value={schoolInput}></input>
+//             <label htmlFor="date-input">Date</label>
+//             <input onChange={handleDate} id="date-input" value={dateInput}></input>
+//             <button>Submit</button>
+//         </form>
+//     )
+// }
+
+// export function EducationRender(){
+//     const [render, setRender] = useState([
+//         <div id={crypto.randomUUID()}>
+//             <EducationForm></EducationForm>
+//             <button onClick={deleteSection}>Delete</button>
+//         </div>
+//     ]);
+
+//     function addSection(){
+//         console.log(render);
+//         // setRender(render.push(
+//         //     <p>Hello</p>
+//         // ));
+//     }
+
+//     function deleteSection(event){
+//         // console.log("deleteSection says:", event.target.parentElement.id);
+//         for (let index = 0; index < render.length; index++) {
                
-        }
-    }
+//         }
+//     }
 
-    return(
-        <div>
-            {render}
-            <button onClick={addSection}>Add Section</button>
-        </div>
-    )
-}
+//     return(
+//         <div>
+//             {render}
+//             <button onClick={addSection}>Add Section</button>
+//         </div>
+//     )
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { EducationRender } from './education'
+import { HandleEducation } from './education'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <EducationRender></EducationRender>
+    <HandleEducation></HandleEducation>
   </StrictMode>,
 )
