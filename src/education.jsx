3158dc5ea7ba5f4b09525499a21ae7fa491c6e42
felipeballywright <felipeballywright@gmail.com>
2025-07-07 10:41:04 -0300

    DAY 3 CV

--- src/education.jsx
+++ src/education.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 
-// Store an array of unique IDs/objects.
-// Use .map() to render <EducationForm /> based on that array.
-// On delete, filter out the ID.
-// Add new sections by appending a new ID.
+// READ THE CODE, MAKE SURE YOU UNDERSTAND EVERY LITTLE NUANCE BEFORE MOVING ON
+// PAY ATTENTION AT THE USE OF PROPS AND HOW THEY ARE PASSED DOWN FROM THE PARENT FUNCTION
 
 function Education({title, school, date}){
     return(
@@ -15,105 +13,53 @@ function Education({title, school, date}){
     )
 }
 
-export function HandleEducation(){
-    console.log("Running HandleEducation");
-    const educationData = [];
-
-    function handleSubmit(){
-
-    }
-
+function EducationForm({title, school, date, onTitleChange, onSchoolChange, onDateChange, handleSubmit}){
     return(
         <form onSubmit={handleSubmit}>
             <label htmlFor="title-input">Title</label>
-            <input id="title-input"></input>
+            <input id="title-input" value={title} onChange={onTitleChange}></input>
+
             <label htmlFor="school-input">School</label>
-            <input id="school-input"></input>
+            <input id="school-input" value={school} onChange={onSchoolChange}></input>
+
             <label htmlFor="date-input">Date</label>
-            <input id="date-input"></input>
+            <input id="date-input" value={date} onChange={onDateChange}></input>
+
             <button>Submit</button>
         </form>
     )
 }
 
-// function EducationForm(){
-//     const [titleInput, setTitleInput] = useState('');
-//     const [schoolInput, setSchoolInput] = useState('');
-//     const [dateInput, setDateInput] = useState('');
-//     const [isSubmitted, setIsSubmitted] = useState(false);
+function EducationRender(){
+    const [sections, setSections] = useState([
+        {id: crypto.randomUUID(), title: "", school: "", date: "", isSubmitted: false}
+    ]);
 
-//     function handleSubmit(){
-//         if(!isSubmitted){
-//             setIsSubmitted(true);
-//         } else if(isSubmitted){
-//             setIsSubmitted(false);
-//         }
-//     }
+    // YOU'RE READY TO RIGHT THE HANDLERS!
 
-//     function handleTitle(event){
-//        setTitleInput(event.target.value);
-//     }
-
-//     function handleSchool(event){
-//        setSchoolInput(event.target.value);
-//     }
-
-//     function handleDate(event){
-//        setDateInput(event.target.value);
-//     }
-
-//     if(isSubmitted){
-//         return(
-//             <>
-//               <Education
-//                 title={titleInput}
-//                 school={schoolInput}
-//                 date={dateInput}
-//               />
-//               <button onClick={handleSubmit}>Edit</button>
-//             </>
-//         )
-//     }
-    
-//     return(
-//         <form onSubmit={handleSubmit}>
-//             <label htmlFor="title-input">Title</label>
-//             <input onChange={handleTitle} id="title-input" value={titleInput}></input>
-//             <label htmlFor="school-input">School</label>
-//             <input onChange={handleSchool} id="school-input" value={schoolInput}></input>
-//             <label htmlFor="date-input">Date</label>
-//             <input onChange={handleDate} id="date-input" value={dateInput}></input>
-//             <button>Submit</button>
-//         </form>
-//     )
-// }
-
-// export function EducationRender(){
-//     const [render, setRender] = useState([
-//         <div id={crypto.randomUUID()}>
-//             <EducationForm></EducationForm>
-//             <button onClick={deleteSection}>Delete</button>
-//         </div>
-//     ]);
-
-//     function addSection(){
-//         console.log(render);
-//         // setRender(render.push(
-//         //     <p>Hello</p>
-//         // ));
-//     }
-
-//     function deleteSection(event){
-//         // console.log("deleteSection says:", event.target.parentElement.id);
-//         for (let index = 0; index < render.length; index++) {
-               
-//         }
-//     }
-
-//     return(
-//         <div>
-//             {render}
-//             <button onClick={addSection}>Add Section</button>
-//         </div>
-//     )
-// }
+    return(
+        <div>
+            {sections.map((section) => {
+                if(section.isSubmitted){
+                    return <Education 
+                        key={section.id}
+                        title={section.title} 
+                        school={section.school} 
+                        date={section.date}
+                    />
+                } else{
+                    return <EducationForm 
+                        key={section.id}
+                        title={section.title} 
+                        school={section.school} 
+                        date={section.date}
+                        onTitleChange={(e) => {onTitleChange(e, section.id)}}
+                        onSchoolChange={(e) => {onSchoolChange(e, section.id)}}
+                        onDateChange={(e) => {onDateChange(e, section.id)}}
+                        handleSubmit={(e) => {handleSubmit(e, section.id)}}
+                    />
+                }
+            })}
+        </div>
+    )
+}
