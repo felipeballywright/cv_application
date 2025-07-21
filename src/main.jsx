343034c82c60ef08f36270df7c7ff2f891a8d916
felipeballywright <felipeballywright@gmail.com>
2025-07-21 10:45:13 -0300

    DAY 9

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GeneralInfoForm } from './general_info'
-import { EducationRender } from './education'
-import { ExperienceRender } from './experience'
 import './index.css'
 import './main.css'  
+// import { BasicInfo } from './cv_logic'
+import { BasicInfoForm } from './cv_logic'
+import { HandleBasicInfo } from './cv_logic'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div id='body-container'>
-      <GeneralInfoForm></GeneralInfoForm>
-    </div>
+    
   </StrictMode>,
 )
